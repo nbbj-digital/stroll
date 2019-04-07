56cb8f884e41b8903b739a3dd7a5a46f2e136ee0
Petr Mitev <p.mitevpi@gmail.com>
2019-04-07 11:39:42 -0400

    spelling fix on import, fixed test

--- src/api/RouteData.js
+++ src/api/RouteData.js
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-const Turf = require('Turf');
+const Turf = require('turf');
 const TurfRandom = require('@Turf/random');
 
 let createGraph = require('ngraph.graph');
